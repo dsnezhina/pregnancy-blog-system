72d5cb0603ba1ac7290717c185e1e98e95760433
dsnezhina <66200617+dsnezhina@users.noreply.github.com>
2020-08-04 19:19:47 +0300

    feat: implement guest/user navigation

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,14 +1,61 @@
-import React from 'react';
-import Link from "../link"
+import React, { useContext } from 'react';
+import Link from "../link";
 import styles from './index.module.css';
-import getNavigation from '../../utils/navigation'
+import UserContext from '../../Context'
+
 
 const Header = () => {
-    const links = getNavigation({ id: "123" });
+    const context = useContext(UserContext);
+    const { user, loggedIn } = context;
+
+    const navigation = {
+        guest: [{
+            title: "❤ Home",
+            link: "/"
+        },
+        {
+            title: "❤ Register",
+            link: "/register"
+        },
+        {
+            title: "❤ Login",
+            link: "/login"
+        }],
+        logged: [{
+            title: "❤ Home",
+            link: "/"
+        },
+        {
+            title: "❤ Contributers",
+            link: "/contributers"
+        },
+        {
+            title: "❤ Publications",
+            link: "/publications"
+        },
+        {
+            title: "❤ Post!",
+            link: "/post"
+        },
+        {
+            title: "❤ Profile",
+            link: `/profile/${user && user.id}`
+        },
+        {
+            title: "❤ Logout",
+            link: "/logout"
+        }]
+    };
+
+    console.log("user", user)
+    //console.log("logged", user.loggedIn)
+
+    const links = loggedIn ? navigation.logged : navigation.guest;
+
     return (
         <header className={styles.navigation}>
             {
-                links.header.map(navElement => {
+                links.map(navElement => {
                     return (
                         <Link
                             key={navElement.title}
