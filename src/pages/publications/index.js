c23b5276895c07186a2272dd61547ed47ff96857
dsnezhina <66200617+dsnezhina@users.noreply.github.com>
2020-08-12 22:39:29 +0300

    fix/categorization

--- src/pages/publications/index.js
+++ src/pages/publications/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useRouteMatch } from 'react-router-dom';
 import Aside from '../../components/aside';
 import Blogposts from '../../components/blogposts'
 import styles from './index.module.css';
@@ -8,8 +8,9 @@ import PageLayout from '../../components/page-layout';
 
 
 const PublicationsPage = () => {
-    const location = useLocation();
-    const category = location.pathname.split('/')[2];
+
+    const match = useRouteMatch();
+    const category = match.params.category || null
 
     return (
         <PageLayout>
