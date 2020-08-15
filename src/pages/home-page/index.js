08677b06e52698c242013d080618b54627c50583
dsnezhina <66200617+dsnezhina@users.noreply.github.com>
2020-08-15 13:10:58 +0300

    fix: fix home page image and css

--- src/pages/home-page/index.js
+++ src/pages/home-page/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Header from '../../components/header';
 import Footer from '../../components/footer';
 import styles from './index.module.css';
-import errorPageImage from '../../images/home-page-image.png';
+import homePageImage from '../../images/home-page-image.png';
 
 function HomePage() {
   return (
     <div className={styles.app}>
       <Header />
-      <img className={styles.img} src={errorPageImage} alt='' />
+      <img className={styles.img} src={homePageImage} alt='' />
       <Footer />
     </div>
   );
